611024b724c112df5e13f65a331659fe5c0158e1
ryo-ARAKI <r_araki@fm.me.es.osaka-u.ac.jp>
2020-04-30 13:36:32 +0900

    dev: add search_circumscribed_circle function

--- circumscribed_ellipse.jl
+++ circumscribed_ellipse.jl
@@ -21,6 +21,15 @@ module ParamVar
         x::Array{Float64}  # x coordinate
         y::Array{Float64}  # y coordinate
     end
+
+    """
+    For circumscribed circle
+    """
+    mutable struct Circle
+        centre_x::Float64
+        centre_y::Float64
+        radius::Float64
+    end
 end
 
 
@@ -47,6 +56,58 @@ using Distributions
         y_rot = x .* sin(param.angle_distribution) + y .* cos(param.angle_distribution)
 
         return x_rot, y_rot
+    end
+
+    """
+    Compute distance between two points
+    """
+    function compute_distance(x1, y1, x2, y2)
+        dist_square = (x1-x2)^2 + (y1-y2)^2
+        return sqrt(dist_square)
+    end
+
+
+    """
+    Find centre of circumscribed circle of given point group
+    """
+    function search_circumscribed_circle(param, points)
+        # Initial guess = corner of region
+        centre_x = -param.x_lim
+        centre_y = -param.x_lim
+
+        # Ratio of movement/distance to the most far point
+        num_move = 100
+        move = 0.5 * param.x_lim
+        dist_max = 0.0
+
+        while move >= 1.0e-4
+            for itr_move = 1:num_move
+                # Find the most far point
+                dist_max = 0.0
+                object_x = 0.0
+                object_y = 0.0
+                for itr_point = 1:param.num_points
+                    dist = compute_distance(
+                        centre_x, centre_y,
+                        points.x[itr_point], points.y[itr_point]
+                    )
+                    if dist > dist_max
+                        dist_max = dist
+                        object_x = points.x[itr_point]
+                        object_y = points.y[itr_point]
+                    end
+                end
+
+                # Move towards the most far point
+                centre_x += move * (object_x - centre_x)
+                centre_y += move * (object_y - centre_y)
+            end
+
+            # Dicrease move ratio
+            move *= 0.5
+        end
+
+        return centre_x, centre_y, dist_max
     end
 end
 
@@ -75,6 +136,48 @@ gr()
 
         savefig(p, "./tmp/distributed_points.png")
     end
+
+
+    """
+    Define circle shape
+    """
+    function circle_shape(centre_x, centre_y, radius)
+        θ = LinRange(0, 2*π, 100)
+        centre_x .+ radius*sin.(θ), centre_y .+ radius*cos.(θ)
+    end
+
+
+    """
+    Plot points & circumscribed circle
+    """
+    function plot_points_circle(param, points, circle)
+        p = scatter(
+            points.x, points.y,
+            markercolor = :black,
+            aspect_ratio = 1,
+            xlims = (-param.x_lim, param.x_lim),
+            ylims = (-param.x_lim, param.x_lim),
+            axis = nothing,
+            size=(640, 640)
+        )
+        p! = scatter!(
+            [circle.centre_x], [circle.centre_y],
+            markercolor = :red
+        )
+        p! = plot!(
+            circle_shape(circle.centre_x, circle.centre_y, circle.radius),
+            seriestype = [:shape,],
+            lw = 0.5,
+            c = :blue,
+            linecolor = :black,
+            legend = false,
+            fillalpha =0.2,
+            aspect_ratio = 1,
+            title = "circumscribed circle"
+        )
+
+        savefig(p!, "./tmp/circumscribed_circle.png")
+    end
 end
 
 # ========================================
@@ -90,9 +193,11 @@ gr(
 )
 using .ParamVar
 using .Compute:
-distribute_points
-using .Output
-
+distribute_points,
+search_circumscribed_circle
+using .Output:
+plot_points,
+plot_points_circle
 
 # ----------------------------------------
 ## Declare parameters
@@ -120,7 +225,7 @@ points.x, points.y = distribute_points(param)
 
 ###CHECK###
 # Plot distribution
-Output.plot_points(param, points)
+plot_points(param, points)
 ###CHECK###
 
 
@@ -129,7 +234,17 @@ Output.plot_points(param, points)
 ## Define centre & radius
 ## By iterative method
 # ----------------------------------------
+centre_x = 0.0
+centre_y = 0.0
+radius = 0.0
+circle = ParamVar.Circle(centre_x, centre_y, radius)
 
+circle.centre_x, circle.centre_y, circle.radius = search_circumscribed_circle(param, points)
+
+###CHECK###
+# Plot distribution
+plot_points_circle(param, points, circle)
+###CHECK###
 
 
 # ----------------------------------------
